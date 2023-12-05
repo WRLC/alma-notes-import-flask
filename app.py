f713447e45d9a559002b7419d7ba7ea9aceee03e
Tom Boone <boone@wrlc.org>
2023-12-05 17:27:27 -0500

    create form

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, flash, redirect, url_for, render_template
+from forms import UploadForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secret!'
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        # Save the file somewhere
+        return form.csv.data.filename
+    return render_template('upload.html', form=form)
 
 
 if __name__ == '__main__':
