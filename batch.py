dc272418aaa1e4eea0f059ecbcc5a9d3392c2289
Tom Boone <boone@wrlc.org>
2023-12-06 11:29:42 -0500

    simplify batch.py

--- batch.py
+++ batch.py
@@ -5,78 +5,30 @@ from settings import ALMA_SERVER, ALMA_API_KEY
 
 
 def batch(csvfile, almafield):
-    with open(csvfile) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        rownumber = 1
+    with open(csvfile) as csv_file:  # Open CSV file
+        csv_reader = csv.reader(csv_file, delimiter=',')  # Open CSV file for reading
+        rownumber = 1  # Initialize row number for error reporting
 
-        # Iterate through each row of the CSV
-        for row in csv_reader:
+        for row in csv_reader:  # Iterate through each row of the CSV file
             barcode = row[0]  # Column 1 = barcode
             note = row[1]  # Column 2 = value to insert as a note
 
-            # Get item record from barcode via requests
-            try:
+            try:  # Get item record from barcode via requests
                 r = requests.get(ALMA_SERVER + '/almaws/v1/items', params={
                     'apikey': ALMA_API_KEY,
                     'item_barcode': barcode,
                     'format': 'json'
                 })
+                r.raise_for_status()  # Provide for reporting HTTP errors
 
-                # Provide for reporting HTTP errors
-                r.raise_for_status()
+            except Exception as errh:  # If error...
+                print('Error finding Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errh)
+                rownumber = rownumber + 1  # Bump the row number up before exiting
+                continue  # Stop processing this row
 
-            except requests.exceptions.HTTPError as errh:
-
-                # If HTTP error, inform user
-                print('HTTP Error finding Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errh)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.ConnectionError as errc:
-
-                # If connection error, inform user
-                print('Connection Error finding Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errc)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.Timeout as errt:
-
-                # If timeout error, inform user
-                print('Timeout Error finding Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errt)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.RequestException as err:
-
-                # If other error, inform user
-                print('Other Error finding Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', err)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            # If request good, parse JSON into a variable
-            itemrec = r.json()
-
-            # Insert column 2 value into the destination field
-            itemrec['item_data'][almafield] = note
-
-            # Specify JSON content type for PUT request
-            headers = {'content-type': 'application/json'}
+            itemrec = r.json()  # If request good, parse JSON into a variable
+            itemrec['item_data'][almafield] = note  # Insert column 2 value into the destination field
+            headers = {'content-type': 'application/json'}  # Specify JSON content type for PUT request
 
             # Get IDs from item record for building PUT request endpoint
             mms_id = itemrec['bib_data']['mms_id']  # Bib ID
@@ -86,61 +38,18 @@ def batch(csvfile, almafield):
             # Construct API endpoint for PUT request from item record data
             putendpoint = '/almaws/v1/bibs/' + mms_id + '/holdings/' + holding_id + '/items/' + item_pid
 
-            # send full updated JSON item record via PUT request
-            try:
+            try:  # send full updated JSON item record via PUT request
                 r = requests.put(ALMA_SERVER + putendpoint, params={
                     'apikey': ALMA_API_KEY
                 }, data=json.dumps(itemrec), headers=headers)
+                r.raise_for_status()  # Provide for reporting HTTP errors
 
-                # Provide for reporting HTTP errors
-                r.raise_for_status()
+            except Exception as errh:  # If error...
+                print('Error updating Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errh)
+                rownumber = rownumber + 1  # Bump the row number up before exiting
+                continue  # Stop processing this row
 
-            except requests.exceptions.HTTPError as errh:
-
-                # If HTTP error, inform user
-                print('HTTP Error updating Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errh)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.ConnectionError as errc:
-
-                # If connection error, inform user
-                print('Connection Error updating Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errc)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.Timeout as errt:
-
-                # If timeout error, inform user
-                print('Timeout Error updating Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', errt)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            except requests.exceptions.RequestException as err:
-
-                # If other error, inform user
-                print('Other Error updating Barcode ' + str(barcode) + ' in row ' + str(rownumber) + ':', err)
-
-                # Bump the row number up before exiting
-                rownumber = rownumber + 1
-
-                # Stop processing this row
-                continue
-
-            # Bump the row number up before going to next row
-            rownumber = rownumber + 1
+            rownumber = rownumber + 1  # Bump the row number up before going to next row
 
     # Provide import info as output to command line
     print('Import complete. All submitted barcodes (except any errors listed above) have been updated in Alma.')
